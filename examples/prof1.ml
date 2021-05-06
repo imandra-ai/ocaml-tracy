fe6b5dbcf0127142d2d871dd3c127725e1fc506c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-05-06 11:50:26 -0400

    update example a bit

--- examples/prof1.ml
+++ examples/prof1.ml
@@ -1,15 +1,15 @@
 
 module T = Tracy
 
-let main_loop i n =
-  T.name_thread (Printf.sprintf "thread_%d" i);
+let main_loop th_n n =
+  T.name_thread (Printf.sprintf "thread_%d" th_n);
   for _i = 0 to n do
     let _sp = T.enter ~file:__FILE__ ~line:__LINE__ ~name:"z1" () in
     for _j = 0 to 10 do
       T.with_ ~file:__FILE__ ~line:__LINE__ ~name:"z2" () @@ fun () ->
       Thread.delay 0.05;
 
-      if _j mod 3 = 0 then T.message_f (fun k->k "oh hello from %d!" i);
+      if _j mod 3 = 0 then T.message_f (fun k->k "oh hello from %d (i=%d, j=%d)!" th_n _i _j);
       if _j mod 2 = 0 then T.plot "x" (float (_i * (1+_j)) /. 100.);
     done;
     T.exit _sp;
