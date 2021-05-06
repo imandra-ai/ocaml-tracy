0fe99213e4f62ad534ce5c6cff42a96e52d600a2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-05-06 12:38:17 -0400

    make example more interesting (compute fib… yes)

--- examples/prof1.ml
+++ examples/prof1.ml
@@ -1,16 +1,29 @@
 
 module T = Tracy
 
+let rec fib n =
+  if n<=1 then 1
+  else fib (n-1) + fib (n-2)
+
 let main_loop th_n n =
   T.name_thread (Printf.sprintf "thread_%d" th_n);
-  for _i = 0 to n do
-    let _sp = T.enter ~file:__FILE__ ~line:__LINE__ ~name:"z1" () in
-    for _j = 0 to 10 do
-      T.with_ ~file:__FILE__ ~line:__LINE__ ~name:"z2" () @@ fun () ->
-      Thread.delay 0.05;
+  for i = 1 to n do
+    let _sp = T.enter ~file:__FILE__ ~line:__LINE__ ~name:"outer" () in
+    T.add_text_f _sp (fun k->k "outer loop %d/%d" i n);
 
-      if _j mod 3 = 0 then T.message_f (fun k->k "oh hello from %d (i=%d, j=%d)!" th_n _i _j);
-      if _j mod 2 = 0 then T.plot "x" (float (_i * (1+_j)) /. 100.);
+    for j = 0 to 10 do
+      T.with_ ~file:__FILE__ ~line:__LINE__ ~name:"inner.fib" () @@ fun _sp ->
+      T.set_color _sp 0xaa000f;
+
+      let fib_param = 20 + (i + 4 * j + th_n) mod 19 in
+      T.add_text_f _sp (fun k->k "compute fib(%d)" fib_param);
+
+      let fib_res = fib fib_param in
+      Thread.delay 0.001;
+
+      T.message_f (fun k->k "fib(%d) = %d" fib_param fib_res);
+
+      if j mod 2 = 0 then T.plot "fib" (float fib_res);
     done;
     T.exit _sp;
   done
@@ -21,4 +34,5 @@ let () =
   let n = 100 in
   let l = CCList.init 3 (fun i -> Thread.create (main_loop i) n) in
   List.iter Thread.join l;
+  T.message "all done";
   ()
